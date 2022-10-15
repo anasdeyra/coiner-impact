5df792f5101e406a370cff29ae6814cffd276398
Anass <45261478+anasdeyra@users.noreply.github.com>
2022-10-15 20:24:06 +0100

    SEO image fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,12 +23,12 @@ function App({ Component, pageProps }: AppProps<{ session: Session }>) {
           property="og:description"
           content="Coiner impact is the web3 magazine"
         />
-        <meta
+        {/* <meta
           property="og:image"
           content={
             "https://thumbor.forbes.com/thumbor/fit-in/x/https://www.forbes.com/advisor/in/wp-content/uploads/2022/03/pexels-pixabay-315788-scaled.jpg"
           }
-        />
+        /> */}
         <meta property="og:site_name" content="Coiner impact" />
         {/* twitter tags */}
         <meta name="twitter:card" content="summary" />
@@ -39,12 +39,12 @@ function App({ Component, pageProps }: AppProps<{ session: Session }>) {
           content={"Coiner impact is the web3 magazine"}
         />
         <meta name="twitter:creator" content="@coiner_impact" />
-        <meta
+        {/* <meta
           name="twitter:image"
           content={
             "https://thumbor.forbes.com/thumbor/fit-in/x/https://www.forbes.com/advisor/in/wp-content/uploads/2022/03/pexels-pixabay-315788-scaled.jpg"
           }
-        />
+        /> */}
       </Head>
       <SessionProvider session={pageProps.session}>
         <MantineProvider
